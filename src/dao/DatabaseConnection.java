824b86027199f02df7f1c5cc07868a889011b3fe
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-08-08 22:20:20 -0500

    Create H2 Implementation and add SQL Script, config log4j

---
+++ src/dao/DatabaseConnection.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public static Connection getConnection() throws Exception {
+        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection("jdbc:h2:~/h2db/odontologos", "sa", "");
+    }
+
+    public static Connection startConnection() {
+        Connection conn = null;
+
+        try {
+            conn = getConnection();
+            if (!conn.isClosed()) return conn;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void endConnection(Connection conn)  {
+        try {
+            if (conn != null) conn.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

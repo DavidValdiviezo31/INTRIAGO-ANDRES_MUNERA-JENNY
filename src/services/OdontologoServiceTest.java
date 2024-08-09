940faed02f8d940e2cdf665e2dcb9c153d14b7e0
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-08-08 23:08:31 -0500

    added test to verify first Element (name)

--- src/services/OdontologoServiceTest.java
+++ src/services/OdontologoServiceTest.java
@@ -42,4 +42,23 @@ class OdontologoServiceTest {
     List<Odontologo> listaOdontologos = service.obtenerOdontologos();
     assertEquals(2, listaOdontologos.size());
   }
+
+  @Test
+  void verificarPrimerOdontologo(){
+    OdontologoService service = new OdontologoService(false);
+
+    Odontologo od1 = new Odontologo(35453, "Jenny", "Munera");
+    Odontologo od2 = new Odontologo(65987, "Andres", "Intriago");
+
+    // Act
+    service.guardarOdontologo(od1);
+    service.guardarOdontologo(od2);
+
+    // Assert
+    List<Odontologo> listaOdontologos = service.obtenerOdontologos();
+
+    assertEquals("Jenny",listaOdontologos.get(0).getNombre());
+  }
+
+
 }
